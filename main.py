815af3f591192ec029fe81367c57bd32480c0690
Benjaminunterstrich20 <benniboy827@gmail.com>
2025-06-03 21:33:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'geheimcode123'
 
-# Chats speichern: code -> Liste von Nachrichten (dict mit name + text)
-chats = {}
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route("/", methods=["GET", "POST"])
+rooms = {}  # Raum-Code : list von Usernames
+
+@app.route('/')
 def index():
-    if request.method == "POST":
-        code = request.form.get("code", "").strip()
-        if code:
-            if code not in chats:
-                chats[code] = []
-            return redirect(url_for("chat", code=code))
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/chat/<code>", methods=["GET", "POST"])
-def chat(code):
-    if code not in chats:
-        chats[code] = []
+@app.route('/chat', methods=['POST'])
+def chat():
+    room = request.form.get('room')
+    username = request.form.get('username')
+    if not room or not username:
+        return redirect(url_for('index'))
+    return render_template('chat.html', room=room, username=username)
 
-    msgs = chats[code]
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
 
-    if request.method == "POST":
-        name = request.form.get("name", "").strip()
-        msg = request.form.get("message", "").strip()
-        if name and msg:
-            msgs.append({"name": name, "text": msg})
-            return redirect(url_for("chat", code=code))
+    if room not in rooms:
+        rooms[room] = []
+    rooms[room].append(username)
 
-    return render_template("chat.html", code=code, messages=msgs)
+    emit('system_message', {'msg': f'{username} ist dem Raum beigetreten'}, room=room)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@socketio.on('message')
+def handle_message(data):
+    room = data['room']
+    username = data['username']
+    msg = data['msg']
+    is_image = data.get('is_image', False)
+
+    emit('message', {
+        'username': username,
+        'msg': msg,
+        'is_image': is_image
+    }, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+
+    if room in rooms and username in rooms[room]:
+        rooms[room].remove(username)
+        if len(rooms[room]) == 0:
+            del rooms[room]
+
+    emit('system_message', {'msg': f'{username} hat den Raum verlassen'}, room=room)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
